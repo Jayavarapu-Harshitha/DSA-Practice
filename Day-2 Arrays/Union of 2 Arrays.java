2e5c53223caa3281100dbd27faf774543ba29471
Jayavarapu Lakshmi Venkata Sri Harshitha <87439958+Jayavarapu-Harshitha@users.noreply.github.com>
2022-01-11 20:39:11 +0530

    Create Union of 2 Arrays.java

---
+++ Day-2 Arrays/Union of 2 Arrays.java
@@ -0,0 +1,12 @@
+class Solution{
+    public static int doUnion(int a[], int n, int b[], int m) 
+    {
+        HashSet<Integer> h = new HashSet<>();
+
+        for (int i = 0; i < a.length; i++)
+            h.add(a[i]);
+        for (int i = 0; i < b.length; i++)
+            h.add(b[i]);
+          return h.size();
+    }
+}
